69d2db2dcf768ed208cd84e1720bb2a7c6607775
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-01 17:39:21 -0500

    Time: 408 ms (98.18%), Space: 15.6 MB (24.33%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -1,7 +1,9 @@
+"""
+
+"""
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         left, right = 1, max(piles)
-        minAmount = float("inf")
         while left <= right:
             mid = left + (right - left) // 2
             time = 0
@@ -10,6 +12,5 @@ class Solution:
             if time > h:
                 left = mid + 1
             else:
-                minAmount = min(minAmount, mid)
                 right = mid - 1
-        return minAmount
+        return left
