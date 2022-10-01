f695ef41ea0cbb3d2c8f1e9b7cb1c62619f609b5
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-01 16:53:31 -0500

    Time: 96 ms (94.59%), Space: 14.2 MB (24.93%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,3 +1,7 @@
+"""
+Time = O(log(n))
+Space = O(1)
+"""
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         short, long = nums1, nums2
@@ -8,10 +12,10 @@ class Solution:
         half = total // 2
         left, right = 0, len(short)-1
         
-        while left -1 <= right:
+        while left <= right+1:
+            # Mid of short list
             shortMid = left + (right - left) // 2
             longMid = half - shortMid - 2
-            
             shortL = short[shortMid] if shortMid >= 0 else float("-inf")
             shortR = short[shortMid + 1] if shortMid < len(short) - 1 else float("inf")
             longL = long[longMid] if longMid >= 0 else float("-inf")
