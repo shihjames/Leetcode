ac3e948fb697e25498b387d92e2e7a7dea14a5f9
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-04 02:35:56 -0500

    Time: 98 ms (99.12%), Space: 18.8 MB (35.20%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,11 +1,28 @@
-from collections import Counter
-from heapq import heappush, heappop
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        def quickSelect(left, right):
+            if left == right:
+                return
+            pivot_index = randint(left, right)
+            pivot = unique[pivot_index][0]
+            unique[pivot_index], unique[right] = unique[right], unique[pivot_index]
+
+            i = left
+            for j in range(left, right):
+                if unique[j][0] >= pivot:
+                    unique[i], unique[j] = unique[j], unique[i]
+                    i += 1
+            unique[right], unique[i] = unique[i], unique[right]
+            pivot_index = i
+            if pivot_index == k:
+                return
+            elif pivot_index > k:
+                return quickSelect(left, pivot_index-1)
+            else:
+                return quickSelect(pivot_index+1, right)
+                              
+        k -= 1
         counter = Counter(nums)
-        heap = []
-        for val, count in counter.items():
-            heappush(heap, (count, val))
-            if len(heap) > k:
-                heappop(heap)
-        return [val for count, val in heap]
+        unique = [(count, num) for num, count in counter.items()]
+        quickSelect(0, len(unique)-1)
+        return [num for count, num in unique[:k+1]]
