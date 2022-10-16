0de0f2ab72145599ed12a264f632361edb772445
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-16 00:50:21 -0500

    Time: 192 ms (40.37%), Space: 13.8 MB (73.22%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+        res = [0] * (len(num1)+len(num2))
+        
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                cur = int(num1[i]) * int(num2[j]) + res[i+j]
+                res[i+j] = cur % 10
+                res[i+j+1] += cur // 10
+        
+        if res[-1] == 0:
+            res.pop()
+        
+        return "".join(str(num) for num in reversed(res))
+                
