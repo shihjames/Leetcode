96d2d398a3d35dc8962b0a05ce2fc5c2545ec92e
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-16 23:40:04 -0500

    Time: 64 ms (51.24%), Space: 13.8 MB (77.24%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -3,6 +3,11 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+"""
+Time = O(n)
+Space = O(1)
+"""
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         dummy1 = cur1 = ListNode()
@@ -15,8 +20,8 @@ class Solution:
                 cur2.next = head
                 cur2 = cur2.next
             head = head.next
-        
         cur2.next = None
         cur1.next = dummy2.next
+        
         return dummy1.next
                 
