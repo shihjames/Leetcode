c4b25fb882733b899c61206775c5c5fe944937cd
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-16 23:28:49 -0500

    Time: 201 ms (73.79%), Space: 17.6 MB (32.12%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,18 +4,18 @@ Space = O(n)
 """
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        def dfs(i, j, count):
+        def bfs(i, j, count):
             if i < 0 or i > len(grid)-1 or j < 0 or j > len(grid[0])-1 or grid[i][j] == 0:
                 return count-1
             grid[i][j] = 0
             for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                count = dfs(i+dx, j+dy, count+1)
+                count = bfs(i+dx, j+dy, count+1)
             return count
         
         area = 0
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col] == 1:
-                    cur = dfs(row, col, 1)
+                    cur = bfs(row, col, 1)
                     area = max(area, cur)
         return area
