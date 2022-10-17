796f76e00ca03738f4d286ee7646d401547cb195
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-16 23:39:39 -0500

    Time: 72 ms (27.96%), Space: 13.8 MB (77.24%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        dummy1 = cur1 = ListNode()
+        dummy2 = cur2 = ListNode()
+        while head:
+            if head.val < x:
+                cur1.next = head
+                cur1 = cur1.next
+            else:
+                cur2.next = head
+                cur2 = cur2.next
+            head = head.next
+        
+        cur2.next = None
+        cur1.next = dummy2.next
+        return dummy1.next
+                
