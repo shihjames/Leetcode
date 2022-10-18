84a43368cf12aac2a811f94d4022ef6a2e6dcd0c
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-18 16:28:35 -0500

    Time: 86 ms (14.72%), Space: 14.4 MB (77.30%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -5,20 +5,35 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
+from collections import deque
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        def dfs(cur_node):
-            if cur_node in cloned:
-                return cloned[cur_node]
-            copy = Node(cur_node.val)
-            cloned[cur_node] = copy
-            
-            for neighbor in cur_node.neighbors:
-                copy.neighbors.append(dfs(neighbor)) 
-            return copy
-        
         if not node:
             return None
-        cloned = {}
-        return dfs(node)
+        cloned = {node: Node(node.val)}
+        queue = deque([node])
+        while queue:
+            cur_node = queue.popleft()
+            for neighbor in cur_node.neighbors:
+                if neighbor not in cloned:
+                    cloned[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                cloned[cur_node].neighbors.append(cloned[neighbor])
+        return cloned[node] 
+                
+        
+#     def cloneGraph(self, node: 'Node') -> 'Node':
+#         def dfs(cur_node):
+#             if cur_node in cloned:
+#                 return cloned[cur_node]
+#             copy = Node(cur_node.val)
+#             cloned[cur_node] = copy
+            
+#             for neighbor in cur_node.neighbors:
+#                 copy.neighbors.append(dfs(neighbor)) 
+#             return copy
+        
+#         if not node:
+#             return None
+#         cloned = {}
+#         return dfs(node)
