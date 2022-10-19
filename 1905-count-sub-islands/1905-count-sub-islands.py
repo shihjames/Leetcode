5cc984620a0aa8fea9e35303d5124fb41088ddee
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-19 14:22:43 -0500

    Time: 7449 ms (35.26%), Space: 89.7 MB (35.63%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -3,13 +3,14 @@ class Solution:
         def dfs(row, col, isSubIsland):
             if row < 0 or col < 0 or row >= rows or col >= cols or grid2[row][col] == 0:
                 return isSubIsland
+            cur_status = 1
             grid2[row][col] = 0
             if grid1[row][col] == 0:
-                isSubIsland = 0
+                cur_status = 0
             for dr, dc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
                 if dfs(row + dr, col + dc, isSubIsland) == 0:
-                    isSubIsland = 0
-            return isSubIsland
+                    cur_status = 0
+            return cur_status
         
         rows, cols = len(grid2), len(grid2[0])
         island_count = 0
