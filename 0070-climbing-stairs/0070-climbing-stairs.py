26d6aefbb52f59692b9c590c98188936403d57ed
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-20 22:31:00 -0500

    Time: 57 ms (37.15%), Space: 13.8 MB (57.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1 or n == 2:
+        if n == 0 or n == 1 or n == 2:
             return n
         steps = [0] * (n+1)
         steps[1], steps[2] = 1, 2
