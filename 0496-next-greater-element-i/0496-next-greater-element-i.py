9019f9e46e8b034402012cbb9d272bcadb797bc8
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-23 17:41:58 -0500

    Time: 41 ms (99.55%), Space: 14.2 MB (56.99%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,25 +1,22 @@
+"""
+Time = O(n+m)
+Space = O(n)
+"""
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums2Dict = {} # key: num, value: index
-        for i, num in enumerate(nums2):
-          nums2Dict[num] = i
+        indices = defaultdict(int)
+        res = [-1] * len(nums1)
+        for index, num in enumerate(nums1):
+            indices[num] = index
+        
+        stack = []
+        for i in range(len(nums2)):
+            cur = nums2[i]
+            while stack and cur > stack[-1]:
+                num = stack.pop()
+                if num in indices:
+                    res[indices[num]] = cur
+            stack.append(cur)
 
-        # nums2 = [1, 3, 4, 2]
-        # 		   i  j ->    
-        # Step2:
-        greaterList = [-1] * len(nums2)
-        for i in range(len(nums2) - 1):
-          currNum = nums2[i]
-          for j in range(i + 1, len(nums2)):
-            if currNum < nums2[j]:
-              greaterList[i] = nums2[j]
-              break
-
-        # Step3:
-        result = []
-        for num in nums1:
-          position = nums2Dict[num]
-          result.append(greaterList[position])
-
-        return result
+        return res
             
