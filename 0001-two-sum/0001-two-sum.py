5112b47f580cd90f8343e02867ed9f6f70d78d3e
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-29 11:18:36 -0500

    Time: 113 ms (74.87%), Space: 15.3 MB (14.66%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        remain = {}
+        
+        for i in range(len(nums)):
+            if nums[i] not in remain:
+                remain[target-nums[i]] = i
+            else:
+                return [remain[nums[i]], i]
+        
+        return [-1, -1]
