682b0bb22c7dc0e2b73231ecbb7dfa958098d1c0
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-29 16:19:39 -0500

    Time: 39 ms (92.11%), Space: 14.9 MB (83.21%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,28 +9,25 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        def getClonedNode(node):
+            if node:
+                if node in node_dict:
+                    return node_dict[node]
+                else:
+                    cloned = Node(node.val)
+                    node_dict[node] = cloned
+                    return cloned
+        
         if not head:
             return None
         new_head = Node(head.val)
         node_dict = {head: new_head}
-        cur = head.next
-        new_cur = new_head
-        while cur:
-            new_cur.next = Node(cur.val)
-            node_dict[cur] = new_cur.next
-            new_cur = new_cur.next
-            cur = cur.next
         cur = head
         new_cur = new_head
-        
         while cur:
-            if cur.random:
-                new_cur.random = node_dict[cur.random]
-            else:
-                new_cur.random = None
+            new_cur.next = getClonedNode(cur.next)
+            new_cur.random = getClonedNode(cur.random)
             new_cur = new_cur.next
             cur = cur.next
-        return new_head
         
-            
-            
+        return new_head
