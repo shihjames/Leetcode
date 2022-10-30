933a3c9003bfb57b7208d07c8ad0a4ebff24aa7a
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-30 02:46:43 -0500

    Time: 111 ms (92.07%), Space: 17.8 MB (52.04%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,23 +5,45 @@
 #         self.next = next
 """
 Time = O(nlog(k))
-Space = O(n)
+Space = O(1)
 """
-from heapq import heappush, heappop
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = cur = ListNode()
-        heap = []
-        for i in range(len(lists)):
-            if lists[i]:
-                heappush(heap, (lists[i].val, i))
+        def mergeTwoLists(list1, list2):
+            dummy = cur = ListNode()
+            while list1 and list2:
+                if list1.val < list2.val:
+                    cur.next = list1
+                    list1 = list1.next
+                else:
+                    cur.next = list2
+                    list2 = list2.next
+                cur = cur.next
+            cur.next = list1 if list1 else list2
+            return dummy.next   
         
-        while heap:
-            val, index = heappop(heap)
-            cur.next = ListNode(val)
-            cur = cur.next
-            lists[index] = lists[index].next
-            if lists[index]:
-                heappush(heap, (lists[index].val, index))
+        length = len(lists)
+        if length == 0:
+            return None
+        interval = 1
+        while interval < length:
+            for i in range(0, length-interval, interval*2):
+                lists[i] = mergeTwoLists(lists[i], lists[i+interval])
+            interval *= 2
+        return lists[0]
         
-        return dummy.next
+#         dummy = cur = ListNode()
+#         heap = []
+#         for i in range(len(lists)):
+#             if lists[i]:
+#                 heapq.heappush(heap, (lists[i].val, i))
+        
+#         while heap:
+#             val, index = heapq.heappop(heap)
+#             cur.next = ListNode(val)
+#             cur = cur.next
+#             lists[index] = lists[index].next
+#             if lists[index]:
+#                 heapq.heappush(heap, (lists[index].val, index))
+        
+#         return dummy.next
