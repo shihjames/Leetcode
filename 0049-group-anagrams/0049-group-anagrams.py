70011ef5614c14275e1a3ea15866d2cbf61851b9
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-30 03:18:31 -0500

    Time: 238 ms (40.22%), Space: 20 MB (9.75%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,8 +1,17 @@
+"""
+Time = O(nk)
+space = O(26nk) = O(nk)
+"""
 from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagrams = defaultdict(list)
+        # O(n)
         for s in strs:
-            anagrams[tuple(sorted(s))].append(s)
-        
+            letters = [0] * 26
+            # O(k)
+            for char in s:
+                letters[ord(char) - ord("a")] += 1
+            anagrams[tuple(letters)].append(s)
+            
         return anagrams.values()
