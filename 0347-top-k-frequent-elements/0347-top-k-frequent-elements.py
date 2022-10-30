08ba1136b82c63f1c6aa933a9554365d29656763
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-30 16:21:26 -0500

    Time: 278 ms (17.71%), Space: 19.6 MB (26.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,14 +4,30 @@ Space = O(n+k)
 """
 from heapq import heappush, heappop
 from collections import Counter
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = [[] for i in range(len(nums)+1)]
         counter = Counter(nums)
-        heap = []
         
         for num, count in counter.items():
-            heappush(heap, (count, num))
-            if len(heap) > k:
-                heappop(heap)
+            freq[count].append(num)
+            
+        res = []
+        
+        for i in range(len(freq)-1, 0, -1):
+            if freq[i]:
+                res.extend(freq[i])
+                if len(res) == k:
+                    return res
+        
+        
+#         counter = Counter(nums)
+#         heap = []
+        
+#         for num, count in counter.items():
+#             heappush(heap, (count, num))
+#             if len(heap) > k:
+#                 heappop(heap)
                 
-        return [num for count, num in heap]
+#         return [num for count, num in heap]
