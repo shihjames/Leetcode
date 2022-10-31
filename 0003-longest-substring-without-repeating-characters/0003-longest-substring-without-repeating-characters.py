b2ae5e3be54b40c5513d4ac56b2d0d4cb9d527b5
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-31 15:02:17 -0500

    Time: 125 ms (62.43%), Space: 14 MB (50.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,15 +3,14 @@ class Solution:
         if len(s) == 0:
             return 0
         longest = 1
+        l = 0
         cur_ele = set()
-        # Create a valid interval until it reaches the end
-        left, right = 0, 1
-        cur_ele.add(s[left])
-        while right < len(s):
-            while s[right] in cur_ele:
-                cur_ele.remove(s[left])
-                left += 1
-            longest = max(longest, right - left + 1)
-            cur_ele.add(s[right])
-            right += 1
+        cur_ele.add(s[l])
+        for r in range(1, len(s)):
+            while s[r] in cur_ele:
+                cur_ele.remove(s[l])
+                l += 1
+            cur_ele.add(s[r])
+            longest = max(longest, r - l + 1)
+        
         return longest
