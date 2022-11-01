ade86545fa15bd940cebaf3257be582e3b0b1959
James Shih <68275741+shihjames@users.noreply.github.com>
2022-10-31 20:51:32 -0500

    Time: 111 ms (42.39%), Space: 18.5 MB (98.39%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -4,6 +4,11 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+"""
+Time = O(n)
+Space = O(h)
+"""
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def helper(root):
