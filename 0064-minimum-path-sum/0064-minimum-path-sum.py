26373e50b2c5560fbafe0c5f010a0506baa86231
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-01 16:41:42 -0500

    Time: 276 ms (18.82%), Space: 15.7 MB (53.79%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -16,4 +16,4 @@ class Solution:
                 else:
                     grid[row][col] = min(grid[row][col] + grid[row-1][col], grid[row][col] + grid[row][col-1])
         
-        return grid[-1][-1]
+        return grid[rows-1][cols-1]
