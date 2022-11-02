78dddc713653157f211c1e27e0637d63afa22602
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-02 18:07:05 -0500

    Time: 998 ms (91.92%), Space: 36.3 MB (45.74%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -5,6 +5,7 @@ Space = O(n)
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pos_speed = []
+        # Create position and speed pair
         for i in range(len(position)):
             pos_speed.append((position[i], speed[i]))
         pos_speed.sort()
@@ -15,8 +16,10 @@ class Solution:
         # Reverse sorted order
         for pos, speed in pos_speed[::-1]:
             time = (target - pos) / speed
+            # Check if there is a collision
             if len(stack) >= 1 and time <= stack[-1]:
                 continue
+            # Stack is empty or no collision
             else:
                 stack.append(time)
                 
