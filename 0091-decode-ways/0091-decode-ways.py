b76fb05a59c9c98e10d4040a4c1e9d89b17fd185
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-03 16:21:32 -0500

    Time: 39 ms (86.26%), Space: 14.2 MB (21.19%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -8,17 +8,23 @@ class Solution:
             if index in dp:
                 return dp[index]
             
+            if index == len(s):
+                return 1
+            
             if s[index] == "0":
                 return 0
             
+            if index == len(s) - 1 :
+                return 1
+            
             res = dfs(index + 1)
             
-            if (index + 1) < len(s) and (s[index] == "1" or (s[index] == "2" and int(s[index + 1]) <= 6)):
+            if int(s[index: index + 2]) <= 26:
                 res += dfs(index + 2)
                                        
             dp[index] = res
             return res
             
-        dp = {len(s): 1}
+        dp = {}
         return dfs(0)
         
