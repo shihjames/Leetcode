3181bd6e528c8c0012724367e006b8d72606c50c
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-03 16:37:34 -0500

    Time: 4651 ms (15.49%), Space: 14.1 MB (85.86%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,16 @@
+"""
+Time = O(c*n)
+Space = O(n)
+where n = amount, c = length of coins
+"""
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        
+        return dp[-1] if dp[-1] != float("inf") else -1
+        
