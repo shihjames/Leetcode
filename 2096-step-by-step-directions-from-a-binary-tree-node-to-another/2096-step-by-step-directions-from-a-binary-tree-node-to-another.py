d1552e0add4a60f7beadbfcafc4f3528d124e9fb
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-03 13:22:10 -0500

    Time: 3776 ms (28.35%), Space: 57.4 MB (94.50%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        s_path = None
+        e_path = None
+        queue = deque([(root, "")])
+        while queue:
+            for i in range(len(queue)):
+                node, path = queue.popleft()
+                if node.val == startValue:
+                    s_path = deque(list(path))
+                elif node.val == destValue:
+                    e_path = deque(list(path))
+                if node.left:
+                    queue.append((node.left, path+"L"))
+                if node.right:
+                    queue.append((node.right, path+"R"))
+        
+        
+        while s_path and e_path and s_path[0] == e_path[0]:
+            s_path.popleft()
+            e_path.popleft()
+            
+        return "U" * len(s_path) + "".join(e_path)
+                
+        
