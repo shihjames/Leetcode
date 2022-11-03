6a48ea09f8c8bb0069cc4940dcc29126d13cf04e
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-03 14:30:11 -0500

    Time: 278 ms (67.14%), Space: 15.1 MB (20.70%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,23 @@
+"""
+Time = O(n)
+Space = O(n)
+"""
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        l = r = 0
+        jump = 0
+        # Greedy: find the furthest step in each iteration
+        while r < len(nums) - 1:
+            furthest = 0
+            # Try all possibilities and find the furthest
+            for i in range(l, r + 1):
+                if nums[i] + i > furthest:
+                    furthest = nums[i] + i
+            # Update new interval
+            l = r + 1
+            r = furthest
+            # Update number of jumps
+            jump += 1
+        
+        return jump
+        
