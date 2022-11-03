746688f282e7607b2a55d888a516e70b5f39020c
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-03 16:05:32 -0500

    Time: 67 ms (31.29%), Space: 14.1 MB (31.00%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        def dfs(index):
+            if index in dp:
+                return dp[index]
+            
+            if s[index] == "0":
+                return 0
+            
+            res = dfs(index + 1)
+            
+            if (index + 1) < len(s) and (s[index] == "1" or (s[index] == "2" and int(s[index + 1]) <= 6)):
+                res += dfs(index + 2)
+                                       
+            dp[index] = res
+            return res
+            
+        dp = {len(s): 1}
+        return dfs(0)
+        
