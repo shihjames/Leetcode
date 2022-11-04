fdcbc9124cfcc3f87597e6ba710a0c9bfae21deb
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-04 18:36:13 -0500

    Time: 697 ms (13.97%), Space: 21.8 MB (31.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,28 @@
-"""
-Time = O(n)
-Space = O(1)
-"""
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * len(nums)
+        """
+        First create two accumulate multiplication arrays
+        [1, nums[0], nums[0]*nums[1], nums[0]*nums[1]*nums[2]]
+        [nums[3]*nums[2]*nums[1], nums[3]*nums[2], nums[3], 1]]
         
-        for i in range(1, len(nums)):
-            res[i] = res[i-1] * nums[i-1]
+        Multiple them according to their indices
+        """
+        # Create to accumulate multiplication arrays
+        left = [1] * len(nums)
+        right = [1] * len(nums)
         
-        product = nums[i]
-        for i in range(len(nums)-2, -1, -1):
-            res[i] *= product
-            product *= nums[i]
+        # Left
+        for i in range(1, len(left)):
+            left[i] = nums[i-1] * left[i-1]
+        # Right
+        for i in range(len(right)-2, -1, -1):
+            right[i] = nums[i+1] * right[i+1]
             
-        return res
+        # Multiple two arrays according to their indices
+        for i in range(len(left)):
+            left[i] *= right[i]
+            
+        return left
+        
+        
         
