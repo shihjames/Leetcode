8553f610752d33dd65642ed9c8692158db17c059
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-03 22:33:39 -0500

    Time: 236 ms (23.00%), Space: 17.6 MB (18.61%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        def dfs(node, prev):
+            if node in visited:
+                return False
+            visited.add(node)
+            for neighbor in neighbors[node]:
+                if neighbor != prev:
+                    if not dfs(neighbor, node):
+                        return False
+            return True
+            
+        visited = set()
+        neighbors = defaultdict(list)
+        for start, end in edges:
+            neighbors[start].append(end)
+            neighbors[end].append(start)
+        
+        return dfs(0, -1) and len(visited) == n
+        
+        
