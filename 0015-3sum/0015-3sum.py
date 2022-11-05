da80f787099c25b79f2aad4c562dccc6d19cbc6f
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-04 20:01:20 -0500

    Time: 639 ms (97.88%), Space: 18.1 MB (40.17%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,18 +1,43 @@
-"""
-Time = O(n**2)
-Space = O(n)
-"""
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = set()
-        dups = set()
-        sum_dict = defaultdict(int)
+        """
+        input: nums: List[int], contains duplicates, unsorted
+        output: res: List[List[int]], every possible combination, return [] if not found
+        1. Sort the array -> skip duplicates
+        2. Traverse through the numbers
+        2. Use two pointers to find valid combinations 
+        [-1, 0, 0, 1, 1, 1, 2, 3] -> [-1, 0, 1]
+          C  L  R
+        """
+        def findComb(index):
+            left = index + 1
+            right = len(nums) - 1
+            
+            # Traverse numbers after current num
+            while left < right:
+                # Count the sum of the combination
+                total = nums[index] + nums[left] + nums[right]
+                if total > 0:
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    res.append([nums[index], nums[left], nums[right]])
+                    while left < right and nums[left+1] == nums[left]:
+                        left += 1
+                    left += 1
+            
+        nums.sort()
+        res = []
+        # Traverse through the array
         for i in range(len(nums)):
-            if nums[i] not in dups:
-                dups.add(nums[i])
-                for j in range(i+1, len(nums)):
-                    complement = -(nums[i] + nums[j])
-                    if complement in sum_dict and sum_dict[complement] == i:
-                        res.add(tuple(sorted([nums[i], nums[j], complement])))
-                    sum_dict[nums[j]] = i
+            # Break if its larger than 0
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+                
+            findComb(i)
+        
         return res
+            
