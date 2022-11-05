ff06d4e3593976cf81ef5387908247583fa05cc3
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-04 19:33:25 -0500

    Time: 125 ms (20.12%), Space: 14.5 MB (44.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,16 +1,24 @@
-"""
-Time = O(n)
-Space = O(1)
-"""
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        left, right = 0, len(s)-1
+        """
+        1. Use two pointers, left (forward) and right (backward)
+        2. Check elements which are pointed by the two pointers are the same
+        (Skip non-alphabetic and non-numeric elements)
+        """
+        
+        left, right = 0, len(s) - 1
+        
         while left < right:
+            # Make sure two pointers point to valid elements
             while left < right and not s[left].isalnum():
                 left += 1
             while left < right and not s[right].isalnum():
                 right -= 1
+                
             if s[left].lower() != s[right].lower():
                 return False
-            left, right = left + 1, right - 1
+            else:
+                left += 1
+                right -= 1
+        
         return True
