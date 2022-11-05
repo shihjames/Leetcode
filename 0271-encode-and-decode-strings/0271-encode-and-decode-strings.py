5c4058217346b4a047b3cfab806931f1d0692a3f
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-04 19:13:27 -0500

    Time: 186 ms (20.16%), Space: 14.5 MB (21.32%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -1,16 +1,32 @@
+"""
+Time = O(n)
+Space = O(n)
+"""
 class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        return chr(12232).join(strs)
+        ret = ""
+        for s in strs:
+            ret += str(len(s)) + "#" + s
+        return ret
         
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        return s.split(chr(12232))
-        
-
+        ret = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i: j])
+            ret.append(s[j + 1: j + 1 + length])
+            i = j + 1 + length
+            
+        return ret
+            
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
