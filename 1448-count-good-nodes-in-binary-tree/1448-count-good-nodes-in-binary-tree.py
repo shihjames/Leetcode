64794c922b384cf2c179a45a3b2a9fce0d8dbb9a
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-06 16:21:39 -0600

    Time: 597 ms (44.68%), Space: 32.4 MB (87.61%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -11,42 +11,41 @@ Space = O(1)
 """
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        
+        def dfs(node, cur_max):
+            nonlocal good_node_cnt
+            
+            if not node:
+                return
+            
+            if node.val >= cur_max:
+                good_node_cnt += 1
+            
+            cur_max = max(cur_max, node.val)
+            dfs(node.left, cur_max)
+            dfs(node.right, cur_max)
+            
         good_node_cnt = 0
-        queue = deque([(root, -float("inf"))])
-        
-        while queue:
-            for i in range(len(queue)):
-                node, cur_max = queue.popleft()
-                if node.val >= cur_max:
-                    good_node_cnt += 1
-                    cur_max = node.val
-                
-                if node.left:
-                    queue.append((node.left, cur_max))
-                
-                if node.right:
-                    queue.append((node.right, cur_max))
-                    
+        dfs(root, -float("inf"))
         return good_node_cnt
-                
-                
-#         def dfs(node, cur_max):
-#             nonlocal good_node_cnt
-            
-#             if not node:
-#                 return
-            
-#             if node.val >= cur_max:
-#                 good_node_cnt += 1
-            
-#             cur_max = max(cur_max, node.val)
-#             dfs(node.left, cur_max)
-#             dfs(node.right, cur_max)
-            
-#         good_node_cnt = 0
-#         dfs(root, -float("inf"))
-#         return good_node_cnt
     
+#         if not root:
+#             return 0
+        
+#         good_node_cnt = 0
+#         queue = deque([(root, -float("inf"))])
+        
+#         while queue:
+#             for i in range(len(queue)):
+#                 node, cur_max = queue.popleft()
+#                 if node.val >= cur_max:
+#                     good_node_cnt += 1
+#                     cur_max = node.val
+                
+#                 if node.left:
+#                     queue.append((node.left, cur_max))
+                
+#                 if node.right:
+#                     queue.append((node.right, cur_max))
+                    
+#         return good_node_cnt
+                
