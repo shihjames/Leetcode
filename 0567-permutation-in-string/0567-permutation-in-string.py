5181ad58d889ef47c5946fd3b1273ad180500967
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-06 01:53:04 -0500

    Time: 183 ms (51.94%), Space: 14.1 MB (31.50%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,45 +1,46 @@
 """
-Time O(26) + O(n) = O(n)
-Space = O(26) = O(1)
+Time = O(n)
+Space = O(1)
 """
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
         
-        count_1 = [0] * 26
-        count_2 = [0] * 26
-        matches = 0
+        match = 0
+        s1_count = [0] * 26
+        s2_count = [0] * 26
         
         for i in range(len(s1)):
-            count_1[ord(s1[i]) - ord("a")] += 1
-            count_2[ord(s2[i]) - ord("a")] += 1
+            s1_count[ord(s1[i]) - ord("a")] += 1
+            s2_count[ord(s2[i]) - ord("a")] += 1
         
-        for i in range(26):
-            if count_1[i] == count_2[i]:
-                matches += 1
-                
+        for i in range(len(s1_count)):
+            if s1_count[i] == s2_count[i]:
+                match += 1
+        
         left = 0
         for right in range(len(s1), len(s2)):
-            if matches == 26:
+            if match == 26:
                 return True
             
             index = ord(s2[right]) - ord("a")
-            count_2[index] += 1
-            if count_1[index] == count_2[index]:
-                matches += 1
-            elif count_1[index] + 1 == count_2[index]:
-                matches -= 1
-        
+            s2_count[index] += 1
+            if s1_count[index] == s2_count[index]:
+                match += 1
+            elif s1_count[index] + 1 == s2_count[index]:
+                match -= 1
+            
             index = ord(s2[left]) - ord("a")
-            count_2[index] -= 1
-            if count_1[index] == count_2[index]:
-                matches += 1
-            elif count_1[index] - 1 == count_2[index]:
-                matches -= 1
-                
+            s2_count[index] -= 1
+            if s1_count[index] == s2_count[index]:
+                match += 1
+            elif s1_count[index] - 1 == s2_count[index]:
+                match -= 1
+            
             left += 1
         
-        return matches == 26
+        return match == 26
             
+                
                 
