2101a7eecbdbcba4edb7206ef7538c25eeba0cd7
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-05 23:19:06 -0500

    Time: 76 ms (25.15%), Space: 14.2 MB (40.29%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,18 +1,34 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        """
+        1. add a left parentheses
+        2. check number of ")" is no more than "(", add a right parentheses
+        
+        recursive function(left_count, right_count, string)
+        
+        if length of string equals to n * 2
+            add string to result
+        
+        if left_count is smaller than n
+            recursive function(left_count + 1, right_count, string + "(")
+        
+        if right_count is smaller than left_count
+            recursive function(left_count, right_count + 1, string + ")")
+        """
         res = []
-        # Recursive function for generating parentheses
-        def helper(left, right, cur_str):
-            # Length of cur_str equals to n -> Finish generating
+        
+        def generateHelper(left_cnt, right_cnt, cur_str):
             if len(cur_str) == n * 2:
                 res.append(cur_str)
-                return
-            # check numbers of left paren and right paren:
-            if left < n:
-                helper(left + 1, right, cur_str + "(")
-            if right < left:
-                helper(left, right + 1, cur_str + ")")
-         
-        helper(0, 0, "")
+                
+            if left_cnt < n:
+                generateHelper(left_cnt + 1, right_cnt, cur_str + "(")
+                
+            if right_cnt < left_cnt:
+                generateHelper(left_cnt, right_cnt + 1, cur_str + ")")
+                
+        generateHelper(0, 0, "")
+        
         return res
+                
         
