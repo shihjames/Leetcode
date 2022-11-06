5d5ab277c6eecba41aa415cf9b774b94f6c901b8
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-06 15:21:08 -0600

    Time: 42 ms (80.03%), Space: 13.8 MB (75.65%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -4,43 +4,14 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-"""
-Time = O(n)
-Space = O(h)
-"""
-from collections import deque
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        def check(p_node, q_node):
-            if not p_node and not q_node:
+        def dfs(node1, node2):
+            if node1 is None and node2 is None:
                 return True
-            if not p_node or not q_node:
-                return False
-            if p_node.val != q_node.val:
-                return False
-            else:
-                return True
-            
-        queue = deque([(p, q)])
-        while queue:
-            p_node, q_node = queue.popleft()
-            if not check(p_node, q_node):
+            if (node1 is None or node2 is None) or node1.val != node2.val:
                 return False
             
-            if p_node and q_node:
-                queue.append((p_node.left, q_node.left))
-                queue.append((p_node.right, q_node.right))
+            return dfs(node1.left, node2.left) and dfs(node1.right, node2.right)
         
-        return True
-        
-#         if not p and not q: 
-#             return True
-        
-#         if not p or not q:
-#             return False
-        
-#         if p.val != q.val:
-#             return False
-        
-#         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return dfs(p, q)
