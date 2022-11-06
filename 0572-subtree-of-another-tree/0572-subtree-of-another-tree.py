2884a3b7aa7c0706d5b470d97b2fd2f8a5f54ecc
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-06 15:29:28 -0600

    Time: 374 ms (7.98%), Space: 15.1 MB (64.49%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -4,32 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-"""
-Time = O(m*n)
-Space = O(m+n)
-"""
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         def dfs(node):
             if not node:
                 return False
-            elif sameTree(node, subRoot):
+                
+            if sameTree(node, subRoot):
                 return True
-            return self.isSubtree(node.left, subRoot) or self.isSubtree(node.right, subRoot)
-        
-        def sameTree(p, q):
-            if not p and not q:
+            
+            return dfs(node.left) or dfs(node.right)
+            
+        def sameTree(node1, node2):
+            if node1 is None and node2 is None:
                 return True
-
-            if not p or not q:
+            
+            if (node1 is None or node2 is None) or (node1.val != node2.val):
                 return False
-
-            if p.val != q.val:
-                return False
-
-            return sameTree(p.left, q.left) and sameTree(p.right, q.right)
+            
+            return sameTree(node1.left, node2.left) and sameTree(node1.right, node2.right)
         
         return dfs(root)
-        
         
