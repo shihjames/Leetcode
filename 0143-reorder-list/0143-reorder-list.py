983e573b7588a49b6bafcfdc8f7b328732c04994
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-06 02:14:08 -0600

    Time: 236 ms (17.44%), Space: 24.2 MB (13.46%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -3,39 +3,39 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-"""
-Time = O(n)
-Space = O(1)
-"""
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow = fast = head
+        slow = head
+        fast = head
+        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
+         
         cur = slow.next
         slow.next = None
-        rev = None
+        tail = None
         while cur:
-            nex = cur.next
-            cur.next = rev
-            rev = cur
-            cur = nex
+            next_node = cur.next
+            cur.next = tail
+            tail = cur
+            cur = next_node
+        
         
         dummy = cur = ListNode()
-        while rev:
+        while tail:
             cur.next = head
             head = head.next
             cur = cur.next
-            cur.next = rev
-            cur = cur.next
-            rev = rev.next
-        cur.next = head
-        return dummy.next
             
+            cur.next = tail
+            tail = tail.next
+            cur = cur.next
+            
+        cur.next = head
         
+        return dummy.next
         
