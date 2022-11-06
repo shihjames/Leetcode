33176a5b2810dfb109de4517487a4e83a2644052
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-06 02:16:21 -0600

    Time: 125 ms (30.61%), Space: 17.7 MB (31.53%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -3,17 +3,17 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-"""
-Time = O(n)
-Space = O(1)
-Alternate solution: hashmap
-"""
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = fast = head
+        slow = head
+        fast = head
+        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+            
             if slow is fast:
                 return True
+        
         return False
