de008727fec27b3116d5bf7562f1a8bf4a45296c
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-05 23:19:51 -0500

    Time: 84 ms (13.23%), Space: 14.3 MB (40.29%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,22 +1,5 @@
 class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        """
-        1. add a left parentheses
-        2. check number of ")" is no more than "(", add a right parentheses
-        
-        recursive function(left_count, right_count, string)
-        
-        if length of string equals to n * 2
-            add string to result
-        
-        if left_count is smaller than n
-            recursive function(left_count + 1, right_count, string + "(")
-        
-        if right_count is smaller than left_count
-            recursive function(left_count, right_count + 1, string + ")")
-        """
-        res = []
-        
+    def generateParenthesis(self, n: int) -> List[str]:        
         def generateHelper(left_cnt, right_cnt, cur_str):
             if len(cur_str) == n * 2:
                 res.append(cur_str)
@@ -26,7 +9,8 @@ class Solution:
                 
             if right_cnt < left_cnt:
                 generateHelper(left_cnt, right_cnt + 1, cur_str + ")")
-                
+               
+        res = []
         generateHelper(0, 0, "")
         
         return res
