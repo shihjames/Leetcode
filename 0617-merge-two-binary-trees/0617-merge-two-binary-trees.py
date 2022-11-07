2c4ff0bc57f0fdbbb4c055ad96016ef13a9a99c7
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-06 22:25:05 -0600

    Time: 226 ms (12.51%), Space: 15.5 MB (81.60%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -4,6 +4,11 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+"""
+Time = O(n)
+Space = O(h)
+"""
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root1 and not root2:
