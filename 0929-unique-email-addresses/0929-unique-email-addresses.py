d41005ff2d38d0c703eda90adddb6efb1d37ff17
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-12 15:53:37 -0600

    Time: 55 ms (93.71%), Space: 13.9 MB (97.37%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -1,14 +1,16 @@
+"""
+Time = O(nm)
+Space = O(nm)
+"""
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
         email_set = set()
         
         for email in emails:
-            email_split = email.split("@")
-            local = email_split[0]
-            domain = email_split[1]
+            local, domain = email.split("@")
             target = local.split("+")[0]
             alias = "".join(target.split("."))
-            email_set.add((alias, domain))
+            email_set.add((alias+"@"+domain))
         
         return len(email_set)
             
