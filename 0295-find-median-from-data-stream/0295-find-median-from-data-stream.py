741c7aa3f727a3402e30ad2d022fd6c4104d094d
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-12 15:12:10 -0600

    Time: 1289 ms (56.19%), Space: 36.4 MB (12.22%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -0,0 +1,28 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []
+
+    def addNum(self, num: int) -> None:
+        heappush(self.max_heap, -num)
+        heappush(self.min_heap, -heappop(self.max_heap))
+        if len(self.min_heap) > len(self.max_heap):
+            heappush(self.max_heap, -heappop(self.min_heap))
+
+
+    def findMedian(self) -> float:
+        # Even
+        if len(self.min_heap) == len(self.max_heap):
+            return (self.min_heap[0] + -self.max_heap[0]) / 2
+        # Odd
+        else:
+            return -self.max_heap[0]
+            
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
