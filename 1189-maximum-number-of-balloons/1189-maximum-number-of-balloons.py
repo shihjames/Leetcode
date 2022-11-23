b1a85d181fdf1c0d967633fb0bcd032a8d25a4d0
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-23 01:33:34 -0600

    Time: 37 ms (90.72%), Space: 13.9 MB (52.89%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,13 +1,17 @@
+"""
+Time = O(n)
+Space = O(n)
+"""
 from collections import Counter
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        char_dicT = {"b": 1, "a": 1, "l": 2, "o": 2, "n": 1}
+        char_dict = {"b": 1, "a": 1, "l": 2, "o": 2, "n": 1}
         counter = Counter(text)
         max_num = float("inf")
         
-        for char in char_dicT:
+        for char in char_dict:
             if char not in counter:
                 return 0
-            max_num = min(max_num, counter[char] // char_dicT[char])
+            max_num = min(max_num, counter[char] // char_dict[char])
                 
         return max_num
