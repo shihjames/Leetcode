2bb5ef08563dcda1e63542df50cb9b8048a4e51a
James Shih <68275741+shihjames@users.noreply.github.com>
2022-11-23 01:03:17 -0600

    Time: 110 ms (41.41%), Space: 14.3 MB (14.38%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,15 +1,19 @@
 """
-Time = O(n+m)
+Time = O(n)
 Space = O(n)
 """
+from collections import defaultdict
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # O(n)
         indices = defaultdict(int)
-        res = [-1] * len(nums1)
         for index, num in enumerate(nums1):
             indices[num] = index
         
+        res = [-1] * len(nums1)
+        
         stack = []
+        # O(n)
         for i in range(len(nums2)):
             cur = nums2[i]
             while stack and cur > stack[-1]:
@@ -17,6 +21,10 @@ class Solution:
                 if num in indices:
                     res[indices[num]] = cur
             stack.append(cur)
-
+        
         return res
+                    
+                
+        
+        
             
