0df28a7ab2af825a2cd0ed6cf29716c31973f181
James Shih <68275741+shihjames@users.noreply.github.com>
2022-12-26 16:48:37 -0600

    Time: 505 ms (86.54%), Space: 15.2 MB (82.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,42 +1,8 @@
-"""
-Time = O(n)
-Space = O(1)
-"""
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        # Set goal to the last index
-        goal = len(nums)-1
-        # Traverse backward
-        for i in range(len(nums)-1, -1, -1):
-            # If current index can reach the goal
-            if nums[i] + i >= goal:
-                # Update new goal position
-                goal = i
+        last_index = len(nums) - 1
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] + i >= last_index:
+                last_index = i
         
-        # Check goal position is at the first index
-        return goal == 0
-        
-#         def jumpHelper(index):
-#             # Able to jump to the destination
-#             if index >= len(nums)-1:
-#                 return True
-#             # Simply use the previous result
-#             if index in memo:
-#                 return memo[index]
-#             # Try all jumps
-#             res = False
-#             for i in range(nums[index], 0, -1):
-#                 if jumpHelper(index + i):
-#                     res = True
-#                     break
-#             memo[index] = res
-#             return res
-            
-#         if len(nums) == 0:
-#             return False
-#         if len(nums) == 1:
-#             return True
-#         memo = {}
-#         return jumpHelper(0)
-        
-        
+        return last_index == 0
