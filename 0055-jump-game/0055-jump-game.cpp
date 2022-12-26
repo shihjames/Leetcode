d6ed60088b399c8f4da679de1ff8c4a777a15d8a
James Shih <68275741+shihjames@users.noreply.github.com>
2022-12-26 16:58:31 -0600

    Time: 76 ms (82.64%), Space: 48.4 MB (48.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,8 +1,13 @@
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        last_index = len(nums) - 1
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] + i >= last_index:
-                last_index = i
-        
-        return last_index == 0
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int last_index = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums.at(i) + i >= last_index) {
+                last_index = i;
+            }
+        }
+        return last_index == 0;
+    }
+};
