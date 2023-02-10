22443e299d2d1d2e167e7ff8282871c661143867
James Shih <68275741+shihjames@users.noreply.github.com>
2023-02-10 17:15:04 -0600

    Time: 0 ms (100.00%), Space: 7.4 MB (84.04%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            while (left < right && !iswalnum(s[left])) {
+                left++;
+            }
+            while (left < right && !iswalnum(s[right])) {
+                right--;
+            }
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
